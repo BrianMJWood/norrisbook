6c9f697ef2174ff022faa96880eed1954161da4e
Brian Wood <brianmjwood@gmail.com>
2025-05-11 10:50:51 +0100

    adding entry form with validation

--- src/app/Components/entryform/entryform.component.ts
+++ src/app/Components/entryform/entryform.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-entryform',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './entryform.component.html',
-  styleUrl: './entryform.component.scss'
+  styleUrl: './entryform.component.scss',
 })
 export class EntryformComponent {
+  entryForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d{10}$/),
+    ]),
+  });
 
+  onSubmit() {
+    console.log(this.entryForm.value);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { EntrydisplayComponent } from './Components/entrydisplay/entrydisplay.component';
+import { EntryformComponent } from './Components/entryform/entryform.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [EntrydisplayComponent, EntryformComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'norrisbook';
